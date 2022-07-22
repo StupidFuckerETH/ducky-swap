fc75e1b32f9e43530c6d76d6e981aaa8a3467b77
BerylBit <108334269+berylbit-labs@users.noreply.github.com>
2022-07-23 03:10:11 +1000

    copy initial

---
+++ static/js/27.af7419d4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[27],{1195:function(s,e){s.exports={messages:{}}}}]);
+//# sourceMappingURL=27.af7419d4.chunk.js.map
