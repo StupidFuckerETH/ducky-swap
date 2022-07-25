24cbfaf96d4ef8f4229025de3766d719882a1fa1
BerylBit <108334269+berylbit-labs@users.noreply.github.com>
2022-07-25 22:49:42 +1000

    fix network connection

---
+++ static/js/27.2b2a5954.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp@uniswap/interface"]=this["webpackJsonp@uniswap/interface"]||[]).push([[27],{1201:function(s,e){s.exports={messages:{}}}}]);
+//# sourceMappingURL=27.2b2a5954.chunk.js.map
